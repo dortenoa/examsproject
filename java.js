95161b5741b007b47f847cec1c0ec1b4ba297693
jjrcode <jakobjrasmussen@hotmail.com>
2018-01-03 11:24:12 +0100

    Mobile nav osv.

--- java.js
+++ java.js
@@ -1,13 +1,9 @@
-$(window).scroll(function(){
-    var distanceFromTop = $(document).scrollTop();
-    if(distanceFromTop < 150 )
-    {
-        // reduce div height to 250px
-        $('.navibar').css('height', '150' - $(document).scrollTop() + 'px');
-    }
-    else
-    {
-        // set minimum div height to 100px
-        $('.navibar').css('height', '50px');
-    }
-});
+/* BURGER BAR MENU MOBIL */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
